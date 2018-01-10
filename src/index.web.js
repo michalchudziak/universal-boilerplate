b8b2b9260b96ca08c95c73ddb1fa6f0736512ddc
Michal Chudziak <michal.chudziak@gmail.com>
2018-01-10 12:53:56 +0100

    Section #3 - Add new deps, move JS files.

---
+++ src/index.web.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('UniversalBoilerplate', () => App);
+AppRegistry.runApplication('UniversalBoilerplate', { rootTag: document.getElementById('root') });
